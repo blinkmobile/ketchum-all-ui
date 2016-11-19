47ae822bd22f035cce96652e8a128011ee034333
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-19 16:51:20 +1100

    display data at /solutions and /projects

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,11 +1,44 @@
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import {
+  Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn
+} from 'material-ui/Table'
 import React, { Component } from 'react'
+
+import RouteSection from '../components/RouteSection.js'
+
+import './Projects.css'
+
+import { projects } from '../../__tests__/fixtures/data.json'
 
 class Projects extends Component {
   render () {
     return (
-      <section className=''>
-        <h1>Projects</h1>
-      </section>
+      <RouteSection>
+        <Table multiSelectable>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>Owner</TableHeaderColumn>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Service</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            { projects.map((project) => (
+              <TableRow key={project.id}>
+                <TableRowColumn>{project.ownerTenant}</TableRowColumn>
+                <TableRowColumn>{project.id}</TableRowColumn>
+                <TableRowColumn>{project.name}</TableRowColumn>
+                <TableRowColumn>{project.service}</TableRowColumn>
+              </TableRow>
+            )) }
+          </TableBody>
+        </Table>
+        <FloatingActionButton className='ProjectAddFAB'>
+          <ContentAdd />
+        </FloatingActionButton>
+      </RouteSection>
     )
   }
 }
--- src/routes/Solutions.js
+++ src/routes/Solutions.js
@@ -1,11 +1,42 @@
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import {
+  Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn
+} from 'material-ui/Table'
 import React, { Component } from 'react'
+
+import RouteSection from '../components/RouteSection.js'
+
+import './Solutions.css'
+
+import { solutions } from '../../__tests__/fixtures/data.json'
 
 class Solutions extends Component {
   render () {
     return (
-      <section className=''>
-        <h1>Solutions</h1>
-      </section>
+      <RouteSection>
+        <Table multiSelectable>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>Owner</TableHeaderColumn>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            { solutions.map((solution) => (
+              <TableRow key={solution.id}>
+                <TableRowColumn>{solution.ownerTenant}</TableRowColumn>
+                <TableRowColumn>{solution.id}</TableRowColumn>
+                <TableRowColumn>{solution.name}</TableRowColumn>
+              </TableRow>
+            )) }
+          </TableBody>
+        </Table>
+        <FloatingActionButton className='SolutionAddFAB'>
+          <ContentAdd />
+        </FloatingActionButton>
+      </RouteSection>
     )
   }
 }
