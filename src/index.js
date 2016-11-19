e01ce55eabe79ebff195992ec2a9b39ad4763411
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-19 12:56:57 +1100

    add material-ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 
 import App from './App.js'
 import './index.css'
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path='/' component={App} />
-  </Router>,
+  <MuiThemeProvider>
+    <Router history={hashHistory}>
+      <Route path='/' component={App} />
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
