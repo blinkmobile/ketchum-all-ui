00483d4a5f6158570269777d3ec24c321d207cc3
Ron Waldon <jokeyrhyme@gmail.com>
2016-11-19 13:20:16 +1100

    add Drawer with toggle button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import cn from 'classnames'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
 import React, { Component } from 'react'
 
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.handleMenuButtonTap = this.handleMenuButtonTap.bind(this)
+    this.state = {
+      isDrawerOpen: true
+    }
+  }
+
+  handleMenuButtonTap () {
+    this.setState({ isDrawerOpen: !this.state.isDrawerOpen })
+  }
+
   render () {
+    const { isDrawerOpen } = this.state
     return (
       <div className='App'>
-        <AppBar title='Ketchum-All Registry' />
+        <Drawer docked open={isDrawerOpen} width={256} />
 
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className={cn('Main', isDrawerOpen && 'Main-drawer-open')}>
+          <AppBar onLeftIconButtonTouchTap={this.handleMenuButtonTap} title='Ketchum-All Registry' />
+
+          <p className='App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </main>
       </div>
     )
   }
