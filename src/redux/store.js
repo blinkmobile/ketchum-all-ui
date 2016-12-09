a5765bcf27112444076e21df9442a640678e8f87
Ron Waldon <jokeyrhyme@gmail.com>
2016-12-09 16:26:01 +1100

    integrate with Redux DevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,10 @@ import { createSelector } from 'reselect'
 import reducer from './reducer.js'
 import { getRouting } from './reducers/routing.js'
 
-export const store = createStore(reducer)
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState: createSelector(getRouting, (routing) => routing.toJS())
