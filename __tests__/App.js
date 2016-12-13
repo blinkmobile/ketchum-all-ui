dca223b5075011652bf234cb9d65df4c45644920
Ron Waldon <jokeyrhyme@gmail.com>
2016-12-13 13:25:13 +1100

    fix tests

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-import App from '../src/App'
+import { shallow } from 'enzyme'
+
+import App from '../src/App.js'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  shallow(<App />)
 })
