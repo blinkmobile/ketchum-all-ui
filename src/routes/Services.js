bb4e207c63944204a5b59bcedd780dbd641f05fb
Ron Waldon <jokeyrhyme@gmail.com>
2016-12-14 11:48:59 +1100

    able to show and delete services

--- src/routes/Services.js
+++ src/routes/Services.js
@@ -1,36 +1,119 @@
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
+import { Map, Set } from 'immutable'
+import ActionDelete from 'material-ui/svg-icons/action/delete-forever'
 import ContentAdd from 'material-ui/svg-icons/content/add'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
-import React, { Component } from 'react'
+import { TableRow, TableRowColumn } from 'material-ui/Table'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
-import FlexGrid from '../components/FlexGrid.js'
+import {
+  resourceMapToArray, rowIndicesToResourceIds
+} from '../lib/rows.js'
+import {
+  deleteSelectedServices, requestServices, selectServices
+} from '../redux/actions/services.js'
+import {
+  getServicesMap, getSelectedServices
+} from '../redux/reducers/services.js'
+import { getTenantsMap } from '../redux/reducers/tenants.js'
+import ResourceTable from '../components/ResourceTable.js'
 import RouteSection from '../components/RouteSection.js'
 
 import './Services.css'
 
-import { projects, services } from '../../__tests__/fixtures/data.json'
-
 class Services extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  componentDidMount () {
+    this.props.requestServices()
+  }
+
+  handleSelect (rowsSelected) {
+    const { selectServices, servicesMap } = this.props
+    const selectedServices = rowIndicesToResourceIds(
+      servicesMap,
+      rowsSelected
+    )
+    selectServices(selectedServices)
+  }
+
   render () {
+    const {
+      children, servicesMap, deleteSelectedServices, selectedServices, tenantsMap
+    } = this.props
+
+    const tableProps = {
+      headings: [ 'Label', 'Type', 'Tenant', 'Tenancy', 'Note' ],
+      onSelect: this.handleSelect
+    }
+
     return (
       <RouteSection>
-        <FlexGrid>
-          { services.map((service) => (
-            <Card key={service.id}>
-              <CardHeader title={service.name} subtitle={service.description} />
-              <CardActions />
-              <CardText>
-                Projects {projects.filter((project) => project.service === service.id).length}
-              </CardText>
-            </Card>
-          )) }
-        </FlexGrid>
-        <FloatingActionButton className='ServiceAddFAB'>
-          <ContentAdd />
+        <ResourceTable {...tableProps}>
+          { resourceMapToArray(servicesMap).map((service) => {
+            const {
+              id, label, name, note, serviceType, tenancy,
+              tenant: relatedTenant
+            } = service.toJS()
+            let tenantLabel = ''
+            if (relatedTenant && tenantsMap.has(relatedTenant.id)) {
+              tenantLabel = tenantsMap.get(relatedTenant.id).get('label')
+            }
+            return (
+              <TableRow key={id} selected={selectedServices.has(id)}>
+                <TableRowColumn title={name}>{label}</TableRowColumn>
+                <TableRowColumn>{serviceType}</TableRowColumn>
+                <TableRowColumn>{tenantLabel}</TableRowColumn>
+                <TableRowColumn>{tenancy}</TableRowColumn>
+                <TableRowColumn>{note}</TableRowColumn>
+              </TableRow>
+            )
+          }) }
+        </ResourceTable>
+
+        <Link to='/services/new'>
+          <FloatingActionButton className='ServicesAddFAB' title='add'>
+            <ContentAdd />
+          </FloatingActionButton>
+        </Link>
+
+        <FloatingActionButton className='ServicesDeleteFAB' secondary title='delete' onClick={deleteSelectedServices}>
+          <ActionDelete />
         </FloatingActionButton>
+
+        {children}
       </RouteSection>
     )
   }
 }
 
-export default Services
+Services.propTypes = {
+  children: PropTypes.element,
+
+  // mapStateToProps
+  servicesMap: PropTypes.instanceOf(Map),
+  selectedServices: PropTypes.instanceOf(Set),
+  tenantsMap: PropTypes.instanceOf(Map),
+
+  // mapDispatchToProps
+  deleteSelectedServices: PropTypes.func,
+  requestServices: PropTypes.func,
+  selectServices: PropTypes.func
+}
+
+const mapStateToProps = (state) => ({
+  servicesMap: getServicesMap(state),
+  selectedServices: getSelectedServices(state),
+  tenantsMap: getTenantsMap(state)
+})
+const mapDispatchToProps = {
+  deleteSelectedServices,
+  requestServices,
+  selectServices
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Services)
